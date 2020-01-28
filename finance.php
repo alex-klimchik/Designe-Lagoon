9446abe9ba84176ccb17579d6139c99f0c4a21b4
Alex <alex.lytvyn.13@gmail.com>
2020-01-28 09:48:59 +0200

    Add Finance

--- finance.php
+++ finance.php
@@ -10,8 +10,6 @@
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/fh-3.1.6/datatables.min.js"></script>
     <!-- Main CSS for this Page -->
     <link rel="stylesheet" href="css/finance.css">
-    <!--JS Page-->
-    <script type="text/javascript" src="js/finance.js"></script>
 
 </head>
 
@@ -27,38 +25,64 @@
 
             <?php require_once('block/header.php'); ?>
             <main class="main-content bgc-grey-100">
-                <h4 class="c-grey-900 mB-20">Выплаты, заказанные вебмастерами</h4>
+                <h4 class="c-grey-900 mB-20">Выплаты</h4>
+                <div class="gap-20 bd w-100 bgc-white p-20" style="margin-bottom: 20px">
+                    <form method=" " action="">
+                        <fieldset>
+                            <div class="form-group">
+                                <label for="wallet">Ваш кошелек</label>
+                                <input type="text" id="wallet" class="form-control" name="wallet" placeholder="WMR кошелек отсутствует">
+                            </div>
+                            <div class="form-group ">
+                                <label for="sum">Укажите сумму</label>
+                                <input type="text" class="form-control" id="sum" name="sum" placeholder="Сумма">
+                                <div class="text-help">
+                                    <p>Выплата производится на ваш кошелек WMR, указанный в настройках профиля. Минимальная сумма выплаты - 0</p>
+                                </div>
+                            </div>
+                        </fieldset>
+                        <div class="form-group btn-order">
+                            <button type="submit" class="btn btn-primary">Заказать выплату</button>
+                        </div>
+                    </form>
+                </div>
                 <div class="gap-20 bd w-100 bgc-white p-20">
-                    <table id="dataTable" class="mainTable table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>Дата</th>
-                                <th>User ID</th>
-                                <th>Тип кошелька</th>
-                                <th>Счет</th>
-                                <th>Сумма</th>
-                                <th>Оплачен</th>
-                                <th>Действия</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>2019-12-12 12:33:55</td>
-                                <td>699</td>
-                                <td>WebMoney</td>
-                                <td>R333444555111</td>
-                                <td>46650</td>
-                                <td>Нет</td>
-                                <td>
-                                    <div class="set-button">
-                                        <button type="button" class="btn btn-outline-info">Тикет</button>
-                                        <button type="button" class="btn btn-outline-success">Оплатить</button>
-                                        <button type="button" class="btn btn-outline-danger">Отменить</button>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div class="table-wrap">
+                        <table class="mainTable table table-bordered table-hover">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>Дата</th>
+                                    <th>Способ выплаты</th>
+                                    <th>Счет</th>
+                                    <th>Сумма</th>
+                                    <th>Выплачен</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2019-12-12 12:33:55</td>
+                                    <td>WebMoney</td>
+                                    <td>R333444555111</td>
+                                    <td>46 650</td>
+                                    <td>Да</td>
+                                </tr>
+                                <tr>
+                                    <td>2019-12-12 12:33:55</td>
+                                    <td>WebMoney</td>
+                                    <td>R333444555111</td>
+                                    <td>46 650</td>
+                                    <td>Да</td>
+                                </tr>
+                                <tr>
+                                    <td>2019-12-12 12:33:55</td>
+                                    <td>WebMoney</td>
+                                    <td>R111111111111</td>
+                                    <td>46 650</td>
+                                    <td>Да</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 <!-- /.gap-20.bd.w-100.bgc-white.p-20 -->
             </main>
